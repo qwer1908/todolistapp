1dce579d5c1f05c900bf74e959496e4d52ff870f
qwer1908 <kokoko1981@naver.com>
2021-12-15 22:49:27 +0900

    2021/12/15

---
+++ src/components/CreateToDo.tsx
@@ -0,0 +1,39 @@
+import { useForm } from "react-hook-form";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { categoryState, toDoState } from "../atoms";
+
+interface IForm {
+  toDo: string;
+}
+
+function CreateToDo() {
+
+
+  const setToDos = useSetRecoilState(toDoState);
+  const category = useRecoilValue(categoryState);
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const handleValid = ({ toDo }: IForm) => {
+    setToDos((oldToDos) => [
+      { text: toDo, id: Date.now(), category },
+      ...oldToDos,
+    ]);
+    setValue("toDo", "");
+    
+    localStorage.setItem('key', JSON.stringify(savedToDo));
+  };
+  const savedToDo = useRecoilValue(toDoState);
+  
+  return (
+    <form onSubmit={handleSubmit(handleValid)}>
+      <input
+        {...register("toDo", {
+          required: "Please write a To Do",
+        })}
+        placeholder="Write a to do"
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+
+export default CreateToDo;
---
+++ src/components/ToDo.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { useSetRecoilState, useRecoilValue } from "recoil";
+import { Categories, IToDo, toDoState } from "../atoms";
+
+function ToDo({ text, category, id }: IToDo) {
+  const savedToDo = useRecoilValue(toDoState);
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const newToDo = { text, id, category: name as any };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+    localStorage.setItem('key', JSON.stringify(savedToDo));
+  };
+
+  const deleteToDo = () => {  
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+    localStorage.setItem('key', JSON.stringify(savedToDo));
+
+  };
+  return (
+    <li>
+      <span>{text}</span>
+      {category !== Categories.DOING && (
+        <button name={Categories.DOING} onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== Categories.TO_DO && (
+        <button name={Categories.TO_DO} onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== Categories.DONE && (
+        <button name={Categories.DONE} onClick={onClick}>
+          Done
+        </button>
+      )}
+      <button onClick={deleteToDo}>x</button>
+    </li>
+  );
+}
+
+export default ToDo;
